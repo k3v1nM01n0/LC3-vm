0ef1dbfa5b2c6a2114d68ba63bf6844a7abfe306
kevindson <kevinmoino10@gmail.com>
2021-01-13 22:38:09 +0300

    trap routines

--- main.c
+++ main.c
@@ -62,6 +62,17 @@ enum
     FL_NEG = 1 << 2, /* N */
 };
 
+//Trap routines
+enum
+{
+    TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
+    TRAP_OUT = 0x21,   /* output a character */
+    TRAP_PUTS = 0x22,  /* output a word string */
+    TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
+    TRAP_PUTSP = 0x24, /* output a byte string */
+    TRAP_HALT = 0x25   /* halt the program */
+};
+
 
 uint16_t sign_extend(uint16_t x, int bit_count){
     if((x >> (bit_count - 1) & 1)){
@@ -121,43 +132,136 @@ int main(int argc, const char* argv[]){
                 }
                 break;
             case OP_AND:
-                {}
+                {
+                    uint16_t r0 = (ins >> 9) & 0x7;//DR
+                    uint16_t r1 = (ins >> 6) & 0x7;
+                    uint16_t imm_flag = (ins >> 1) & 0x1;
+
+                    if(imm_flag){
+                        uint16_t imm5 = sign_extend(ins & 0x1F, 5);
+                        reg[r0] = reg[r1] & imm5;
+                    }else{
+                       uint16_t r2 = ins & 0x7;
+                       reg[r0] =reg[r1] + reg[r2];
+                    }
+                }
                 break;
             case OP_NOT:
-                {}
+                {
+                    uint16_t r0 = (ins >> 9) & 0x7;//DR
+                    uint32_t r1 = (ins >> 6) & 0x7;
+
+                    reg[r0] = ~reg[r1];
+                    update_flags(r0);
+                }
                 break;
-            case OP_BR:
-                {}
+            case OP_BR: 
+                {
+                    uint16_t pc_offset = sign_extend(ins & 0x1FF, 9);
+                    uint16_t conn_flag = (ins >> 9) & 0x7;
+
+                    if(conn_flag & reg[R_COND]){
+                        reg[R_PC] += pc_offset;
+                    }
+                }
                 break;
             case OP_JMP:
-                {}
+                {
+                    uint16_t long_flag = (ins >> 11) & 1;
+                    reg[R_R7] = reg[R_PC];
+
+                    if(long_flag){
+                        uint16_t long_pc_offset = sign_extend(ins & 0x7FF, 11);
+                        reg[R_PC] += long_pc_offset; 
+                    }else{
+                        uint16_t r1 = (ins >> 6) & 0x7;
+                        reg[R_PC] = reg[r1];
+                    }
+                }
                 break;
             case OP_JSR:
                 {}
                 break;
             case OP_LD:
-                {}
+                {
+                    uint16_t r0 = (ins >> 9) & 0x7;
+                    uint16_t pc_offset = sign_extend(ins & 0x1FF, 9);
+                    reg[r0] = mem_read(reg[R_PC] + pc_offset);
+                    update_flags(r0);
+                }
                 break;
             case OP_LDI:
-                {}
+                {
+                    uint16_t r0 = (ins >> 9)& 0x7;//DR
+                    uint16_t pc_offcet = sign_extend(ins & 0x1FF, 9);
+
+                    reg[r0] = mem_read(mem_read(reg[R_PC] + pc_offcet));
+                    update_flags(r0);
+                }
                 break;
             case OP_LDR:
-                {}
+                {
+                    uint16_t r0 = (ins >> 9)& 0x7;
+                    uint16_t r1 = (ins >> 6)& 0x7;
+                    uint16_t pc_offset = sign_extend(ins & 0x3F, 6);
+                    reg[r0] = reg[R_PC] + pc_offset;
+                    update_flags(r0);            
+                }
                 break;
             case OP_LEA:
-                {}
+                {
+                    uint16_t r0 = (ins >> 9)& 0x7;
+                    uint16_t pc_offset = sign_extend(ins & 0x1FF, 9);
+                    reg[r0] = reg[R_PC] + pc_offset;
+                    update_flags(r0);
+                }
                 break;
             case OP_ST:
-                {}
+                {
+                    uint16_t r0 = (ins >> 9)& 0x7;
+                    uint16_t pc_offset = sign_extend(ins & 0x1FF, 9);
+                    mem_write(mem_read((reg[R_PC] + pc_offset), reg[r0]));
+                }
                 break;
             case OP_STI:
-                {}
+                { 
+                    uint16_t r0 = (ins >> 9)& 0x7;
+                    uint16_t pc_offset = sign_extend(ins & 0x1FF, 9);
+                    mem_write(mem_read((reg[R_PC] + pc_offset), reg[r0]));
+                }
                 break;
             case OP_STR:
-                {}
+                {
+                    uint16_t r0 = (ins >> 9)& 0x7;
+                    uint16_t r1 = (ins >> 6)& 0x7;
+                    uint16_t offset = sign_extend(ins & 0x3F, 6);
+                    mem_write(reg[r1] + offset, reg[r0]);
+                }
                 break;
             case OP_TRAP:
-                {}
+                {
+                    switch(ins & 0xFF)
+                    {
+                        case TRAP_GETC:
+                            {TRAP GETC, 9}
+                            break;
+                        case TRAP_OUT:
+                            {TRAP OUT, 9}
+                            break;
+                        case TRAP_PUTS:
+                            {TRAP PUTS, 8}
+                            break;
+                        case TRAP_IN:
+                            {TRAP IN, 9}
+                            break;
+                        case TRAP_PUTSP:
+                            {TRAP PUTSP, 9}
+                            break;
+                        case TRAP_HALT:
+                            {TRAP HALT, 9}
+                            break;
+                   }
+                }
                 break;
             case OP_RES:
             case OP_RTI:
